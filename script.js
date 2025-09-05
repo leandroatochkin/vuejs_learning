67bf8024d040f02a9a25644d054e7292acf7af30
leandroatochkin <leandro.atochkin@outlook.es>
2025-09-05 14:33:31 -0300

    testing computed values

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ Vue.component('box',{
 
 })
 
+Vue.component('spinner',{
+    props: ['isLoading'],
+    template: `<div class="box" v-if="isLoading">cargando productos...</div>`
+
+})
+
 const app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!',
         fruits: ['Apple', 'Banana', 'Cherry'],
         newFruit: '',
-        products: []
+        products: [],
+        counter: 0,
+        loading: false,
     },
     methods: {
         addFruit(){
@@ -22,9 +30,19 @@ const app = new Vue({
             this.newFruit = ''
         },
         fetchData(){
+            this.loading = true
             fetch('https://fakestoreapi.com/products/')
             .then(res=>res.json())            
             .then(json=>this.products = [...json])
+            .catch(e => console.log(e))
+            .finally(() => this.loading = false)
+        }
+    },
+    computed: {
+        changeColor() {
+        if (this.counter <= 10) return 'green'
+        if (this.counter < 20) return 'yellow'
+        return 'red'
         }
     }
 })
